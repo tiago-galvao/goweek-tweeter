4474d74fbbe4d20c08e26ef9b03c165e46f561f9
Tiago Galvão Almeida <tiportal.11@gmail.com>
2020-03-21 01:42:56 -0300

    fix: resetando o autor do git

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.send('Hello world');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000')
+  console.log('E funciona')
+});
