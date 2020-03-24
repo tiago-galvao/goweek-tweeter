2c08afe04958eade7b4626d20abb2ba2ff66d556
Tiago Galvão Almeida <tiportal.11@gmail.com>
2020-03-24 01:17:27 -0300

    feature: criando integracao com mongodb e montando as controllers e models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,25 @@
-const express = require("express");
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-  return res.send('Hello world');
+mongoose.connect(
+  "mongodb+srv://goweek:goweek123@estudos-tiagoga-n0n6h.mongodb.net/test?retryWrites=true&w=majority",
+  {useNewUrlParser:true}
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
 });
 
-app.listen(3000, () => {
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes"));
+
+server.listen(3000, () => {
   console.log('Servidor rodando na porta 3000')
   console.log('E funciona')
 });
-
